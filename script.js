e609a3c7ea5143b96aad0d51360103217b6bebe1
Tyler <tjcrimando@gmail.com>
2024-01-08 05:39:02 -0700

    seeds of actual quality

---
+++ script.js
@@ -0,0 +1,24 @@
+function makeunorderedlist(size){
+    const a = []
+    for (let i = 0; i < size; i++){
+        a.push(i+1)
+    }
+
+    var i = size,
+    j = 0,
+    temp;
+
+    while (i--) {
+
+        j = Math.floor(Math.random() * (i+1));
+
+        // swap randomly chosen element with current element
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+
+    }
+
+    return a;
+
+}
