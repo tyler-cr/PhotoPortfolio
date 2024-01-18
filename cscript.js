eb760320d443f721bb6bf5ad49821e7db61d0627
unknown <Crimando@studentloanstudios.com>
2024-01-18 11:43:25 -0800

    Clipboard update

--- cscript.js
+++ cscript.js
@@ -1,43 +1,3 @@
-function makeunorderedlist(size){
-    const a = [];
-    for (let i = 0; i < size; i++){
-        a.push(i+1);
-    }
-
-    var i = size,
-    j = 0,
-    temp;
-
-    while (i--) {
-
-        j = Math.floor(Math.random() * (i+1));
-
-        // swap randomly chosen element with current element
-        temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-
-    }
-
-    return a;
-
-}
-
-function creatColumns(size, location){
-    const randlist = makeunorderedlist(size);
-    const colsize = Math.floor(size/3);
-    let j = 1;
-    for (let i = 0; i< size; i++){
-        if ((i+1)%colsize == 0 && j < 3){
-            j++;
-        }
-        const image = document.createElement('img');
-        image.src = './images/'+location+'/' + randlist[i] + '.jpg';
-        document.getElementById('column'+j).appendChild(image);
-    }
-}
-
-
 // toggle icon navbar
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
@@ -65,3 +25,11 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
+// This should be copying "Poggers" to clipboard
+let copyEmailButton = document.querySelector('.button');
+
+copyEmailButton.onclick = () => {
+    navigator.clipboard.writeText("Tjcrimando@gmail.com");
+    console.log("Buttonn Clicked");
+    }
